7a1a4bf4998372fcd0ef22cceb02d84a0f3c9acf
tun0001 <65064150+tun0001@users.noreply.github.com>
2025-05-28 13:01:50 +0000

    second commit

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ sheet_id = "1y--2Lzvpgk8O7MlgBUZiLJq0LKIoglfNWaBG_bHfynk"
 sheet = client.open_by_key(sheet_id).sheet1  # 1番目のシートを取得
 # スプレッドシートからデータ取得
 #sheet = client.open("YourSpreadsheetName").sheet1
-records = sheet.get_all_records()
-
-# Slack メッセージ整形
-message = "*今週の報告*\n"
-for row in records[-5:]:  # 最新の5件
-    message += f"- {row['日付']}: {row['内容']}\n"
+cell_value1 = sheet.acell("G8").value
+cell_value2 = sheet.acell("G9").value
+print(cell_value1, cell_value2)
+#records = sheet.get_all_records()
+message = f"今週の報告: {cell_value1} - {cell_value2}"
+# # Slack メッセージ整形
+# message = "*今週の報告*\n"
+# for row in records[-5:]:  # 最新の5件
+#     message += f"- {row['日付']}: {row['内容']}\n"
 
 # Slack 投稿
 slack_token = os.environ["SLACK_BOT_TOKEN"]
