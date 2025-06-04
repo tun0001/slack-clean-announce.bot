0ecffe142d41a529f1faa5f1a8cfdb18422753bc
tun0001 <65064150+tun0001@users.noreply.github.com>
2025-06-04 14:22:29 +0000

    line commit

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ name_id_dict = {"日高": "@f.hidaka", "佐々木": "@j.sasaki", "岩本": "@k.i
             "Li": "@s.li", "福田": "@t.fukuda", "柳田": "@k.yanagida", "徳永": "@t.tokunaga", "木下": "@y.kinoshita", "小林": "@t.kobayashi",
            "服部": "@h.hattori", "加藤": "@t.kato", "伊藤": "@k.ito", "岡部": "@ai.okabe", "尾崎": "@m.ozaki", "手代木": "@m.teshirogi", "森山": "@h.moriyama", "矢端": "@h.yabata"}
 
-print(cell_value1, cell_value2)
+#print(cell_value1, cell_value2)
 #records = sheet.get_all_records()
 message = f"【{cleen_date}掃除連絡】\nD455:{name_id_dict[D455_1]} {name_id_dict[D455_2]}\n実験室:{name_id_dict[experiment_1]} {name_id_dict[experiment_2]} \nF322:{name_id_dict[F322_1]} {name_id_dict[F322_2]}\n\n・掃除・ゴミ出し一回ごとに日付を記入\nhttps://docs.google.com/spreadsheets/d/1y--2Lzvpgk8O7MlgBUZiLJq0LKIoglfNWaBG_bHfynk/edit?gid=343667719#gid=343667719\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能い"
 # # Slack メッセージ整形
