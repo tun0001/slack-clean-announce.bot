0697d879689a4f77af8782f2fb2b8ef7eb5d50f9
tun0001 <65064150+tun0001@users.noreply.github.com>
2025-11-05 03:02:21 +0000

    [add] H402

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ experiment_1 = sheet.acell("I8").value
 experiment_2 = sheet.acell("I9").value
 F322_1 = sheet.acell("K8").value
 F322_2 = sheet.acell("K9").value
+H402_1 = sheet.acell("M8").value
+H402_2 = sheet.acell("M9").value
 
 
 name_id_dict = {"日高": "@f.hidaka", "佐々木": "@j.sasaki", "岩本": "@k.iwamoto", "野村":"@k.nomura", "甲斐野": "@s.kaino", "山田": "@y.yamada", 
@@ -29,7 +31,7 @@ name_id_dict = {"日高": "@f.hidaka", "佐々木": "@j.sasaki", "岩本": "@k.i
 
 #print(cell_value1, cell_value2)
 #records = sheet.get_all_records()
-message = f"【{cleen_date}掃除連絡】\nD455:  {name_id_dict[D455_1]}  {name_id_dict[D455_2]}\n実験室:  {name_id_dict[experiment_1]}  {name_id_dict[experiment_2]} \nF322:  {name_id_dict[F322_1]}  {name_id_dict[F322_2]}\n\n・掃除・ゴミ出し一回ごとに日付を記入\nhttps://docs.google.com/spreadsheets/d/1y--2Lzvpgk8O7MlgBUZiLJq0LKIoglfNWaBG_bHfynk/edit?gid=343667719#gid=343667719\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能"
+message = f"【{cleen_date}掃除連絡】\nD455:  {name_id_dict[D455_1]}  {name_id_dict[D455_2]}\n実験室:  {name_id_dict[experiment_1]}  {name_id_dict[experiment_2]} \nF322:  {name_id_dict[F322_1]}  {name_id_dict[F322_2]}\nH402(New): {name_id_dict[H402_1]} {name_id_dict[H402_2]}\n\n・掃除・ゴミ出し一回ごとに日付を記入\nhttps://docs.google.com/spreadsheets/d/1y--2Lzvpgk8O7MlgBUZiLJq0LKIoglfNWaBG_bHfynk/edit?gid=343667719#gid=343667719\n\n・所要時間が30分を超える場合は30分毎に日付を記入(例：1h15minなら3回日付を記入)\n・二人分掃除を行った場合は，二回分記入してください．\n・掃除後はsoro_cleanに作業者名を明記の上完了報告\n・研究室のためのボランティア作業(棚の組み立て等)も記入可能"
 # # Slack メッセージ整形
 # message = "*今週の報告*\n"
 # for row in records[-5:]:  # 最新の5件
